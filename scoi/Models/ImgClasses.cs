50aab899b76f01e776e37b6af3e064eb00df9af8
Andrey Gavrilov <faktorial5@gmail.com>
2020-01-21 17:06:03 +0300

    working...

--- scoi/Models/ImgClasses.cs
+++ scoi/Models/ImgClasses.cs
@@ -64,6 +64,7 @@ namespace scoi.Models
             int width = input.Width;
             int height = input.Height;
 
+
             Bitmap _tmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
             using (Graphics g = Graphics.FromImage(_tmp))
             {
@@ -132,23 +133,35 @@ namespace scoi.Models
         {
             int width = input.Width;
             int height = input.Height;
-            using Bitmap _tmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            byte[] old_bytes = new byte[width * height * 3];
-            byte[] new_bytes = new byte[width * height * 3];
-            byte[] furier_ma_bytes = new byte[width * height * 3];
-            byte[] furier_im_bytes = new byte[width * height * 3];
+            
+            int new_width = width;
+            int new_height = height;
+            
+            var p = Math.Log2(width);
+            if (p != Math.Floor(p))
+                new_width = (int)Math.Pow(2, Math.Ceiling(p));
+            p = Math.Log2(height);
+            if (p != Math.Floor(p))
+                new_height = (int)Math.Pow(2, Math.Ceiling(p));
 
-            double[] spec_im = new double[width * height];
-            double[] spec_re = new double[width * height];
-            double[] spec_ma = new double[width * height];
+            using Bitmap _tmp = new Bitmap(new_width, new_height, PixelFormat.Format24bppRgb);
+            byte[] old_bytes = new byte[new_width * new_height * 3];
+            byte[] new_bytes = new byte[new_width * new_height * 3];
+            byte[] furier_ma_bytes = new byte[new_width * new_height * 3];
+            byte[] furier_im_bytes = new byte[new_width * new_height * 3];
+
+            double[] spec_im = new double[new_width * new_height];
+            double[] spec_re = new double[new_width * new_height];
+            double[] spec_ma = new double[new_width * new_height];
 
 
 
             using Graphics g = Graphics.FromImage(_tmp);
             g.DrawImageUnscaled(input, 0, 0);
-            
 
-            var _tmp_data = _tmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, _tmp.PixelFormat);
+
+
+            var _tmp_data = _tmp.LockBits(new Rectangle(0, 0, new_width, new_height), ImageLockMode.ReadOnly, _tmp.PixelFormat);
             Marshal.Copy(_tmp_data.Scan0, old_bytes, 0, old_bytes.Length);
             _tmp.UnlockBits(_tmp_data);
 
@@ -163,19 +176,19 @@ namespace scoi.Models
             
             job.progress = 0;
 
-            Complex[] complex_bytes = new Complex[width * height];
+            Complex[] complex_bytes = new Complex[new_width * new_height];
             for (int color = 0; color <= 2; color++)
             {
                 
-                for (int i = 0; i < width * height; ++i)
+                for (int i = 0; i < new_width * new_height; ++i)
                 {
-                    int y = i / width;
-                    int x = i - y * width;
+                    int y = i / new_width;
+                    int x = i - y * new_width;
                     complex_bytes[i] = Math.Pow(-1,x+y)*old_bytes[i * 3 + color];
                 }
 
 
-                complex_bytes = FFT.ditfft2d(complex_bytes, width, height);
+                complex_bytes = FFT.ditfft2d(complex_bytes, new_width, new_height);
                 job.progress += 16;
 
 
@@ -183,13 +196,13 @@ namespace scoi.Models
                 //var max_im = complex_bytes.Max(x => Math.Log10(x.Imaginary+1.0));
 
                /* var max_ma = Math.Log10(complex_bytes[1].Real + 1.0);
-                for (int i = 2; i < width * height; ++i)
+                for (int i = 2; i < new_width * height; ++i)
                 {
                     if (max_ma <  Math.Log10(complex_bytes[i].Real + 1.0))
                         max_ma = Math.Log10(complex_bytes[i].Real + 1.0);
                 }*/
 
-                for (int i = 0; i < width * height; ++i)
+                for (int i = 0; i < new_width * new_height; ++i)
                 {
                     spec_re[i] += complex_bytes[i].Real * 1.0 / 3.0;
                     spec_im[i] += complex_bytes[i].Imaginary * 1.0 / 3.0;
@@ -198,23 +211,23 @@ namespace scoi.Models
 
                 var complex_bytes_filtered = complex_bytes.Select((a, i) =>
                 {
-                    int y = i / width;
-                    int x = i - y * width;
+                    int y = i / new_width;
+                    int x = i - y * new_width;
                     foreach (var v in filter_params_double)
                     {
-                        if ((x - v[0] * width) * (x - v[0] * width) + (y - v[1] * height) * (y - v[1] * height) > v[2] * v[2] * height * height &&
-                            (x - v[0] * width) * (x - v[0] * width) + (y - v[1] * height) * (y - v[1] * height) <= v[3] * v[3] * height * height)
+                        if ((x - v[0] * new_width) * (x - v[0] * new_width) + (y - v[1] * new_height) * (y - v[1] * new_height) > v[2] * v[2] * new_height * new_height &&
+                            (x - v[0] * new_width) * (x - v[0] * new_width) + (y - v[1] * new_height) * (y - v[1] * new_height) <= v[3] * v[3] * new_height * new_height)
                             return a*in_filter_zone;
                     }
                     return a * out_filter_zone;
                 }).ToArray();
 
-                var complex_bytes_result = FFT.ditifft2d(complex_bytes_filtered, width, height);
+                var complex_bytes_result = FFT.ditifft2d(complex_bytes_filtered, new_width, new_height);
 
-                for (int i = 0; i < width * height; ++i)
+                for (int i = 0; i < new_width * new_height; ++i)
                 {
-                    int y = i / width;
-                    int x = i - y * width;
+                    int y = i / new_width;
+                    int x = i - y * new_width;
                     new_bytes[i * 3 + color] = clmp(Math.Round( (Math.Pow(-1,x+y) * complex_bytes_result[i]).Real));
                     furier_ma_bytes[i * 3 + color] = clmp(Math.Log(complex_bytes[i].Magnitude + 1.0, 100) * 255.0 / max_ma);
                     
@@ -226,9 +239,9 @@ namespace scoi.Models
             /*using StreamWriter sw = new StreamWriter("fur.csv");
             for (int i = 0; i < height; ++i)
             {
-                for (int j = 0; j < width; ++j)
+                for (int j = 0; j < new_width; ++j)
                 {
-                    sw.Write("\""+ spec_re[i*width+j].ToString() + "\",");
+                    sw.Write("\""+ spec_re[i*new_width+j].ToString() + "\",");
                 }
 
                 sw.Write("\n");
@@ -236,17 +249,23 @@ namespace scoi.Models
             sw.Close();
            */
 
-            Bitmap new_bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            var new_bitmap_data = new_bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, new_bitmap.PixelFormat);
+            using Bitmap new_bitmap = new Bitmap(new_width, new_height, PixelFormat.Format24bppRgb);
+            var new_bitmap_data = new_bitmap.LockBits(new Rectangle(0, 0, new_width, new_height), ImageLockMode.WriteOnly, new_bitmap.PixelFormat);
             Marshal.Copy(new_bytes, 0, new_bitmap_data.Scan0, new_bytes.Length);
             new_bitmap.UnlockBits(new_bitmap_data);
 
-            Bitmap new_bitmap_re = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            var new_bitmap_re_data = new_bitmap_re.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, new_bitmap.PixelFormat);
+            Bitmap new_bitmap_re = new Bitmap(new_width, new_height, PixelFormat.Format24bppRgb);
+            var new_bitmap_re_data = new_bitmap_re.LockBits(new Rectangle(0, 0, new_width, new_height), ImageLockMode.WriteOnly, new_bitmap.PixelFormat);
             Marshal.Copy(furier_ma_bytes, 0, new_bitmap_re_data.Scan0, furier_ma_bytes.Length);
             new_bitmap_re.UnlockBits(new_bitmap_re_data);
 
-            return (new_bitmap, new_bitmap_re);
+            Bitmap new_bitamp_ret = new Bitmap(width,height, PixelFormat.Format24bppRgb);
+            using (Graphics g1 = Graphics.FromImage(new_bitamp_ret))
+            {
+                g1.DrawImageUnscaled(new_bitmap,0,0);
+            }
+            
+            return (new_bitamp_ret, new_bitmap_re);
 
         }
     }
